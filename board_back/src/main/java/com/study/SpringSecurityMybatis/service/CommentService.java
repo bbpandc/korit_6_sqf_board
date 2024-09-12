861a1c714ab7bab2bc5372e1b1e5a9ff1232c0a5
bbpandc <haeminsia@gmail.com>
2024-09-12 17:48:32 +0900

    게시글 조회

--- board_back/src/main/java/com/study/SpringSecurityMybatis/service/CommentService.java
+++ board_back/src/main/java/com/study/SpringSecurityMybatis/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.study.SpringSecurityMybatis.service;
 
+import com.study.SpringSecurityMybatis.dto.request.ReqModifyCommentDto;
 import com.study.SpringSecurityMybatis.dto.request.ReqWriteCommentDto;
 import com.study.SpringSecurityMybatis.dto.response.RespCommentDto;
 import com.study.SpringSecurityMybatis.entity.Comment;
@@ -32,13 +33,21 @@ public class CommentService {
                 .build();
     }
 
+    public void modifyComment(ReqModifyCommentDto dto) {
+        accessCheck(dto.getCommentId());
+        commentMapper.updateById(dto.toEntity());
+    }
+
     public void deleteComment(Long commentId) {
+        accessCheck(commentId);
+        commentMapper.deleteById(commentId);
+    }
+
+    private void accessCheck(Long commentId) {
         PrincipalUser principalUser = (PrincipalUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Comment comment = commentMapper.findById(commentId);
         if(principalUser.getId() != comment.getWriterId()) {
             throw new AccessDeniedException();
         }
-
-        commentMapper.deleteById(commentId);
     }
 }
